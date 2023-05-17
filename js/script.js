49ec689b1edf73597a83451a37f6ee105f50a18b
Rafiur Rahman <133845291+rafitanim@users.noreply.github.com>
2023-05-17 15:16:13 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =================================================typing animation====================================*/
+var typed = new Typed(".typing", {
+    strings:["Programmer","AI developer","& Web designer"],
+    typedSpeed:40,
+    Backspeed:80,
+    loop:true
+})
